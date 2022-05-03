38cbf734167e8b59a93d4794641f8cb52abe91b2
April Archibald <aprilsclearskies@gmail.com>
2022-05-02 19:52:23 -0700

    sensei commit

--- ninja.js
+++ ninja.js
@@ -6,10 +6,10 @@ class Ninja {
         this.strength = 3;
     }
     sayName(){
-        console.log('Ninja Name: ' + this.name)
+        console.log('Name: ' + this.name)
     }
     showStats(){
-        console.log(`Ninja Name: ${this.name}`); 
+        console.log(`Name: ${this.name}`); 
         console.log(`Strength: ${this.strength}`); 
         console.log(`Speed: ${this.speed}`); 
         console.log(`Health:${this.health}`);
@@ -24,3 +24,25 @@ const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+
+class Sensei extends Ninja{
+    constructor(name, wisdom){
+        super(name);
+        this.health = 200;
+        this.speed =10;
+        this.strength =10;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        this.drinkSake();
+        console.log('It is never wise to drive and sake')
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
+
+
+
+
